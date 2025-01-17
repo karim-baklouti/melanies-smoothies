c06f253c19ff8a5a8e598bfc1632d8405680adaa
Karim Baklouti <karimbaklouti@outlook.com>
2025-01-17 15:49:04 +0100

    Update streamline_app.py
    dataframe from api

--- streamline_app.py
+++ streamline_app.py
@@ -45,4 +45,4 @@ if time_to_insert:
 
 # New Section to display smoothiefroot nutrition information
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
